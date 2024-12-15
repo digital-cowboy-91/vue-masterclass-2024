b0f9bcd352e6ee44e02411ee9c3c487c230608bc
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-12-15 22:08:23 +0000

    transform standard routing tofilebase routing using unplugin-vue-router library

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
-
 <template>
   <div>
-    <h1>Home Page</h1>
-    <RouterLink to="/projects">Go to Projects</RouterLink>
+    <h1>404 Not Found</h1>
   </div>
 </template>
 
