eef607ee4e32e3ebd5e7c898940ad225d606e249
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-12-16 15:33:30 +0000

    create navbar and sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,14 @@
 <script setup lang="ts">
-import Avatar from './components/ui/avatar/Avatar.vue'
-import AvatarFallback from './components/ui/avatar/AvatarFallback.vue'
-import DropdownMenu from './components/ui/dropdown-menu/DropdownMenu.vue'
-import DropdownMenuContent from './components/ui/dropdown-menu/DropdownMenuContent.vue'
-import DropdownMenuItem from './components/ui/dropdown-menu/DropdownMenuItem.vue'
-import DropdownMenuLabel from './components/ui/dropdown-menu/DropdownMenuLabel.vue'
-import DropdownMenuSeparator from './components/ui/dropdown-menu/DropdownMenuSeparator.vue'
-import DropdownMenuTrigger from './components/ui/dropdown-menu/DropdownMenuTrigger.vue'
-import Input from './components/ui/input/Input.vue'
-import { Icon } from '@iconify/vue'
+import Sidebar from './components/layout/Sidebar.vue'
+import TopNavbar from './components/layout/TopNavbar.vue'
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
-    <form class="w-full h-fit max-w-96 relative">
-      <Icon
-        icon="lucide:search"
-        class="absolute top-[50%] translate-y-[-50%] left-2.5 text-mute-foreground"
-      />
-      <Input class="pl-8" type="text" placeholder="Search..." />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <!-- <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" /> -->
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
---
+++ src/components/layout/SidebarLinks.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+interface Link {
+  title: string
+  to: string
+  icon: string
+}
+
+defineProps<{
+  links: Link[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="{ title, to, icon } in links"
+    :key="title"
+    :to="to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+    exact-active-class="text-primary bg-muted"
+  >
+    <Icon :icon="icon"></Icon>
+    <span class="hidden lg:block text-nowrap">{{ title }}</span>
+  </RouterLink>
+</template>
