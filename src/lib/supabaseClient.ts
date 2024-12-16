82168fcfd6674b5245509b5cef167606685d3a95
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-12-16 12:51:42 +0000

    fetch projects using IIFE, auto gen types from supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+import type { Database } from '@/../supabase/types'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseKey) throw new Error('Supabase URL and/or KEY undefined')
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../supabase/types'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+
+// IIFE => immediately invoked function expression ;(()=>{})()
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.error(error)
+
+  projects.value = data
+})()
+</script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go to Home</RouterLink>
+
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project }}
+      </li>
+    </ul>
   </div>
 </template>
 
